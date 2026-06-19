f90181d55396a8c9ddb337ce09e4c7a3fe93936a
HACK-junejaaas <devjuneja27@gmail.com>
2026-06-19 14:29:27 +0530

    Fix API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+  import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Assume backend is at localhost:5000
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
 });
 
-// Automatically inject JWT authentication token if stored locally
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+  // Automatically inject JWT authentication token if stored locally
+  API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
 
-export default API;
+  export default API;
